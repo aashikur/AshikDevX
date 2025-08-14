5c92fa4cc175d0022bc96b603c773debe9d62dd9
aashikur <mdaashikur@gmail.com>
2025-08-14 23:59:28 +0600

    hide

--- AshikDevX-Client/src/pages/frontend/Home/Home.jsx
+++ AshikDevX-Client/src/pages/frontend/Home/Home.jsx
@@ -42,10 +42,15 @@ const Home = () => {
            </div>
            <div>
             <AboutGrid></AboutGrid>
+
             <BlogPreviewSection/>
+
             <CTAGetInTouch></CTAGetInTouch>
+
             <PricingSection></PricingSection>
+
             <TestimonialsSection></TestimonialsSection>
+
             <WhatsAppCTA></WhatsAppCTA>
            </div>
         </div>
@@ -55,19 +60,19 @@ const Home = () => {
 export default Home;
 
 
-<div className="min-h-screen w-full bg-[#020617] relative">
-  {/* Dark Sphere Grid Background */}
-  <div
-    className="absolute inset-0 z-0"
-    style={{
-      background: "#020617",
-      backgroundImage: `
-        linear-gradient(to right, rgba(71,85,105,0.3) 1px, transparent 1px),
-        linear-gradient(to bottom, rgba(71,85,105,0.3) 1px, transparent 1px),
-        radial-gradient(circle at 50% 50%, rgba(139,92,246,0.15) 0%, transparent 70%)
-      `,
-      backgroundSize: "32px 32px, 32px 32px, 100% 100%",
-    }}
-  />
-     {/* Your Content/Components */}
-</div>
+// {/* <div className="min-h-screen w-full bg-[#020617] relative">
+//   {/* Dark Sphere Grid Background */}
+//   <div
+//     className="absolute inset-0 z-0"
+//     style={{
+//       background: "#020617",
+//       backgroundImage: `
+//         linear-gradient(to right, rgba(71,85,105,0.3) 1px, transparent 1px),
+//         linear-gradient(to bottom, rgba(71,85,105,0.3) 1px, transparent 1px),
+//         radial-gradient(circle at 50% 50%, rgba(139,92,246,0.15) 0%, transparent 70%)
+//       `,
+//       backgroundSize: "32px 32px, 32px 32px, 100% 100%",
+//     }}
+//   />
+//      {/* Your Content/Components */}
+// </div> */}
